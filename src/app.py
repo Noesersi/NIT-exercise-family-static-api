1d636fd128974cf32754f696bc329b6e65171b6e
Noé Ibáñez Torres <132618491+Noesersi@users.noreply.github.com>
2023-09-21 17:34:14 +0000

    Pending changes exported from your codespace

--- src/app.py
+++ src/app.py
@@ -26,14 +26,11 @@ def sitemap():
     return generate_sitemap(app)
 
 @app.route('/members', methods=['GET'])
-def handle_hello():
+def get_members():
 
     # this is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
-    }
+    response_body = members
 
 
     return jsonify(response_body), 200
